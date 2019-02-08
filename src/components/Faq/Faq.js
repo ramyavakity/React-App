42bdd298014ff196d0e5da81f29ecff98dcf6c36
Ramya Vakity <vakityramya1@gmail.com>
2019-02-08 12:42:53 -0600

    Integrated Bulma and created Components

---
+++ src/components/Faq/Faq.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FAQ extends Component {
+  render() {
+    return (
+      <div className="App">
+      MY FQA
+      </div>
+    );
+  }
+}
+
+export default FAQ;
